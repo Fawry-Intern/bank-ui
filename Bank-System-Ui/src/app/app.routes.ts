2aaafd5318f922476a6952fdcac48adf3abc1a84
mustafatarek <mustafatarek412@gmail.com>
2025-03-18 06:39:11 +0200

    initialized Bank Ui

--- Bank-System-Ui/src/app/app.routes.ts
+++ Bank-System-Ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'login',component:LogInComponent},
+    {path:'sign-up',component:SignUpComponent},
+    { path: '**', redirectTo: 'home' }
+];
---
+++ Bank-System-Ui/src/app/models/user/user-details.model.ts
@@ -0,0 +1,9 @@
+export interface UserDetails{
+
+id:Number,
+firstName:String,
+lastName:String,
+email:String,
+phoneNumber:String,
+address:String
+}
