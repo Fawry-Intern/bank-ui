a32e5d10d58597a4c97414b4b498309f35d0b789
Hossam Hatem <7ossam7atem1@gmail.com>
2025-03-22 00:14:58 +0200

    fix: starter screen to login

--- Bank-System-Ui/src/app/app.routes.ts
+++ Bank-System-Ui/src/app/app.routes.ts
@@ -7,14 +7,14 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { ActiveAccountsComponent } from './pages/active-accounts/active-accounts.component';
 import { BlockedAccountsComponent } from './pages/blocked-accounts/blocked-accounts.component';
 
-
 export const routes: Routes = [
-    {path:'home',component:UserDashboardComponent},
-    {path:'profile/:id',component:ProfileComponent},
-    {path:'admin',component:AdminDashboardComponent},
-    {path:'active-account',component:ActiveAccountsComponent},
-    {path:'un-active-account',component:BlockedAccountsComponent},
-    {path:'login',component:LogInComponent},
-    {path:'sign-up',component:SignUpComponent},
-    { path: '**', redirectTo: 'home' }
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'home', component: UserDashboardComponent },
+    { path: 'profile/:id', component: ProfileComponent },
+    { path: 'admin', component: AdminDashboardComponent },
+    { path: 'active-account', component: ActiveAccountsComponent },
+    { path: 'un-active-account', component: BlockedAccountsComponent },
+    { path: 'login', component: LogInComponent },
+    { path: 'sign-up', component: SignUpComponent },
+    { path: '**', redirectTo: 'login' }
 ];
